0725761c5f59389681112cde26530025a4bd1194
Miguel Gamis <miguelgamis@microsoft.com>
2022-02-01 14:24:49 -0800

    remove casting of param to TestCallingState

--- packages/react-composites/tests/browser/call/app/index.tsx
+++ packages/react-composites/tests/browser/call/app/index.tsx
@@ -27,7 +27,7 @@ import { MockCallAdapter } from './mocks/MockCallAdapter';
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
-const mockCallState = false;
+const mockCallState = JSON.parse(params.mockCallState);
 const useFrLocale = Boolean(params.useFrLocale);
 const showCallDescription = Boolean(params.showCallDescription);
 const injectParticipantMenuItems = Boolean(params.injectParticipantMenuItems);
