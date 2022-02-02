a67c4dd75fb8ba79201b9e35622aeaf3bd5992ce
Miguel Gamis <miguelgamis@microsoft.com>
2022-02-01 17:50:15 -0800

    try-catch statement around JSON.parse

--- packages/react-composites/tests/browser/call/app/index.tsx
+++ packages/react-composites/tests/browser/call/app/index.tsx
@@ -27,7 +27,12 @@ import { MockCallAdapter } from './mocks/MockCallAdapter';
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
-const mockCallState = false;
+let mockCallState = undefined;
+try {
+  mockCallState = JSON.parse(params.mockCallState);
+} catch (e) {
+  console.log('Could not parse: ', params.mockCallState);
+}
 const useFrLocale = Boolean(params.useFrLocale);
 const showCallDescription = Boolean(params.showCallDescription);
 const injectParticipantMenuItems = Boolean(params.injectParticipantMenuItems);
