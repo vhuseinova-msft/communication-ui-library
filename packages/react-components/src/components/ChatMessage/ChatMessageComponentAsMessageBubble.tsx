fb0f662618502c50d5465d30eafe39f5e557d5a3
Porter Nan <jiangnanhello@live.com>
2022-03-18 21:01:13 +0000

    Memoize chat bubble to avoid unnecessary re-render

--- packages/react-components/src/components/ChatMessage/ChatMessageComponentAsMessageBubble.tsx
+++ packages/react-components/src/components/ChatMessage/ChatMessageComponentAsMessageBubble.tsx
@@ -26,7 +26,7 @@ type ChatMessageComponentAsMessageBubbleProps = {
 };
 
 /** @private */
-export const ChatMessageComponentAsMessageBubble = (props: ChatMessageComponentAsMessageBubbleProps): JSX.Element => {
+const MessageBubble = (props: ChatMessageComponentAsMessageBubbleProps): JSX.Element => {
   const ids = useIdentifiers();
   const theme = useTheme();
 
@@ -111,3 +111,6 @@ export const ChatMessageComponentAsMessageBubble = (props: ChatMessageComponentA
 
   return chatMessage;
 };
+
+/** @private */
+export const ChatMessageComponentAsMessageBubble = React.memo(MessageBubble);
