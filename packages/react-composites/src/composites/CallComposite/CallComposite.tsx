025ac0edfc33ce35ff66b5e2744902589ac82fb9
mgamis-msft <miguelgamis@microsoft.com>
2022-07-12 12:00:49 -0700

    pause

--- packages/react-composites/src/composites/CallComposite/CallComposite.tsx
+++ packages/react-composites/src/composites/CallComposite/CallComposite.tsx
@@ -194,9 +194,13 @@ export const CallComposite = (props: CallCompositeProps): JSX.Element => {
   } = props;
   useEffect(() => {
     (async () => {
-      await adapter.askDevicePermission({ video: true, audio: true });
-      adapter.queryCameras();
-      adapter.queryMicrophones();
+      if (role === 'Consumer') {
+        await adapter.askDevicePermission({ video: false, audio: true });
+      } else {
+        await adapter.askDevicePermission({ video: true, audio: true });
+        adapter.queryCameras();
+        adapter.queryMicrophones();
+      }
       adapter.querySpeakers();
     })();
   }, [adapter]);
